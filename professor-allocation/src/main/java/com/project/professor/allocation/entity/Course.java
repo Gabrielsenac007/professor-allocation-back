a69ba4b8805f47a10cbe6e887bac3ec919b101c4
Amanda Duarte <138810888+amanduaita@users.noreply.github.com>
2026-08-16 21:56:32 -0300

    Atualizar o Course.java

--- professor-allocation/src/main/java/com/project/professor/allocation/entity/Course.java
+++ professor-allocation/src/main/java/com/project/professor/allocation/entity/Course.java
@@ -1,4 +1,6 @@
 package com.project.professor.allocation.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +14,7 @@ import lombok.Data;
 @Table(name = "course")
 public class Course {
 	
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
