f2c560234207df92a6434e69766c43f57ececf56
mathen <math.z.mhj@gmail.com>
2026-08-20 20:04:52 -0300

    feat: create pageable list for allocations

--- professor-allocation/src/main/java/com/project/professor/allocation/controller/AllocationController.java
+++ professor-allocation/src/main/java/com/project/professor/allocation/controller/AllocationController.java
@@ -4,17 +4,11 @@ import java.util.List;
 
 import com.project.professor.allocation.dto.AllocationCreateDTO;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import com.project.professor.allocation.entity.Allocation;
 import com.project.professor.allocation.service.AllocationService;
@@ -36,21 +30,32 @@ public class AllocationController {
         this.allocationService = allocationService;
     }
 
-    @Operation(summary = "Find all allocations")
-    @ApiResponses({
-    	@ApiResponse(responseCode = "200", description = "OK")
-    })
+//    antiga request find all sem paginação
+//
+//    @Operation(summary = "Find all allocations")
+//    @ApiResponses({
+//    	@ApiResponse(responseCode = "200", description = "OK")
+//    })
+//    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<List<Allocation>> findAll() {
+//        List<Allocation> allocations = allocationService.findAll();
+//        return new ResponseEntity<>(allocations, HttpStatus.OK);
+//    }
+
+    @Operation(summary = "Find all w/ Pages")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Allocation>> findAll() {
-        List<Allocation> allocations = allocationService.findAll();
-        return new ResponseEntity<>(allocations, HttpStatus.OK);
+    public ResponseEntity<List<Allocation>> findAllWithPages(@RequestParam(defaultValue = "0") int page
+            , @RequestParam(defaultValue = "5") int size) {
+        Page<Allocation> pageableList = allocationService.findAllWithPages(page, size);
+
+        return new ResponseEntity<>(pageableList.getContent(), HttpStatus.OK);
     }
 
     @Operation(summary = "Find an allocation")
     @ApiResponses({
-    	@ApiResponse(responseCode = "200", description = "OK"),
-    	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-    	@ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
     })
     @GetMapping(path = "/{allocation_id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Allocation> findById(@PathVariable(name = "allocation_id") Long id) {
@@ -61,8 +66,8 @@ public class AllocationController {
 
     @Operation(summary = "Find allocations by professor")
     @ApiResponses({
-    	@ApiResponse(responseCode = "200", description = "OK"),
-    	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
     })
     @GetMapping(path = "/professor/{professor_id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Allocation>> findByProfessor(@PathVariable(name = "professor_id") Long id) {
@@ -72,8 +77,8 @@ public class AllocationController {
 
     @Operation(summary = "Find allocations by course")
     @ApiResponses({
-    	@ApiResponse(responseCode = "200", description = "OK"),
-    	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
     })
     @GetMapping(path = "/course/{course_id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Allocation>> findByCourse(@PathVariable(name = "course_id") Long id) {
@@ -83,8 +88,8 @@ public class AllocationController {
 
     @Operation(summary = "Save an allocation")
     @ApiResponses({
-    	@ApiResponse(responseCode = "201", description = "Created"),
-    	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
+            @ApiResponse(responseCode = "201", description = "Created"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
     })
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Allocation> save(@RequestBody @Valid AllocationCreateDTO allocation) {
@@ -94,9 +99,9 @@ public class AllocationController {
 
     @Operation(summary = "Update an allocation")
     @ApiResponses({
-    	@ApiResponse(responseCode = "200", description = "OK"),
-    	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-    	@ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
     })
     @PutMapping(path = "/{allocation_id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Allocation> update(@PathVariable(name = "allocation_id") Long id,
@@ -109,8 +114,8 @@ public class AllocationController {
 
     @Operation(summary = "Delete an allocation")
     @ApiResponses({
-    	@ApiResponse(responseCode = "204", description = "No Content"),
-    	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
+            @ApiResponse(responseCode = "204", description = "No Content"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
     })
     @DeleteMapping(path = "/{allocation_id}")
     public ResponseEntity<Void> deleteById(@PathVariable(name = "allocation_id") Long id) {
