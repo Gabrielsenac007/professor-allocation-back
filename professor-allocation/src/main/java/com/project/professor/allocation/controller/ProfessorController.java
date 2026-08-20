ebc6b84251c84c0dbf1aa1ddbc0499ae18fdafaf
mathen <math.z.mhj@gmail.com>
2026-08-20 14:09:38 -0300

    feat: create pageable list professors

--- professor-allocation/src/main/java/com/project/professor/allocation/controller/ProfessorController.java
+++ professor-allocation/src/main/java/com/project/professor/allocation/controller/ProfessorController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.project.professor.allocation.dto.ProfessorRegisterDTO;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -37,6 +38,8 @@ public class ProfessorController {
         this.professorService = professorService;
     }
 
+    // metodo anterior que listava todos os professores sem paginação
+    /*
     @Operation(summary = "Find all professors")
     @ApiResponses({
     	@ApiResponse(responseCode = "200", description = "OK")
@@ -46,12 +49,22 @@ public class ProfessorController {
         List<Professor> professors = name != null ? professorService.findAll() : professorService.findByName(name);
         return new ResponseEntity<>(professors, HttpStatus.OK);
     }
+    */
+
+    @Operation(summary = "Pageable List Professors")
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Professor>> getPages(@RequestParam(defaultValue = "0") int page
+            , @RequestParam(defaultValue = "5") int size) {
+        Page<Professor> pageableList = professorService.findAllWithPages(page, size);
+
+        return new ResponseEntity<>(pageableList.getContent(), HttpStatus.OK);
+    }
 
     @Operation(summary = "Find a professor")
     @ApiResponses({
-    	@ApiResponse(responseCode = "200", description = "OK"),
-    	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-    	@ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
     })
     @GetMapping(path = "/{professor_id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Professor> findById(@PathVariable(name = "professor_id") Long id) {
@@ -61,8 +74,8 @@ public class ProfessorController {
 
     @Operation(summary = "Find professors by department")
     @ApiResponses({
-    	@ApiResponse(responseCode = "200", description = "OK"),
-    	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
     })
     @GetMapping(path = "/department/{department_id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Professor>> findByDepartment(@PathVariable(name = "department_id") Long id) {
@@ -72,20 +85,20 @@ public class ProfessorController {
 
     @Operation(summary = "Save a professor")
     @ApiResponses({
-    	@ApiResponse(responseCode = "201", description = "Created"),
-    	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
+            @ApiResponse(responseCode = "201", description = "Created"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
     })
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Professor> save(@RequestBody @Valid ProfessorRegisterDTO professor) {
-            Professor professorSaved = professorService.save(professor);
-            return new ResponseEntity<>(professorSaved, HttpStatus.CREATED);
+        Professor professorSaved = professorService.save(professor);
+        return new ResponseEntity<>(professorSaved, HttpStatus.CREATED);
     }
 
     @Operation(summary = "Update a professor")
     @ApiResponses({
-    	@ApiResponse(responseCode = "200", description = "OK"),
-    	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-    	@ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
     })
     @PutMapping(path = "/{professor_id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Professor> update(@PathVariable(name = "professor_id") Long id,
@@ -99,8 +112,8 @@ public class ProfessorController {
 
     @Operation(summary = "Delete a professor")
     @ApiResponses({
-    	@ApiResponse(responseCode = "204", description = "No Content"),
-    	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
+            @ApiResponse(responseCode = "204", description = "No Content"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
     })
     @DeleteMapping(path = "/{professor_id}")
     public ResponseEntity<Void> deleteById(@PathVariable(name = "professor_id") Long id) {
--- professor-allocation/src/main/java/com/project/professor/allocation/service/ProfessorService.java
+++ professor-allocation/src/main/java/com/project/professor/allocation/service/ProfessorService.java
@@ -1,4 +1,5 @@
 package com.project.professor.allocation.service;
+
 import java.util.List;
 
 import com.project.professor.allocation.dto.ProfessorRegisterDTO;
@@ -6,6 +7,9 @@ import com.project.professor.allocation.exceptions.Allocation.InvalidProfessorEx
 import com.project.professor.allocation.exceptions.AlreadyExistsException;
 import com.project.professor.allocation.exceptions.Department.InvalidDepartmentException;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import com.project.professor.allocation.entity.Department;
 import com.project.professor.allocation.entity.Professor;
@@ -14,77 +18,89 @@ import com.project.professor.allocation.Repository.ProfessorRepository;
 @Service
 public class ProfessorService {
 
-	private final com.project.professor.allocation.Repository.ProfessorRepository professorRepository;
-	private final DepartmentService departmentService;
+    private final com.project.professor.allocation.Repository.ProfessorRepository professorRepository;
+    private final DepartmentService departmentService;
 
-	public ProfessorService(com.project.professor.allocation.Repository.ProfessorRepository professorRepository, DepartmentService departmentService) {
-		this.professorRepository = professorRepository;
-		this.departmentService = departmentService;
-	}
+    public ProfessorService(com.project.professor.allocation.Repository.ProfessorRepository professorRepository, DepartmentService departmentService) {
+        this.professorRepository = professorRepository;
+        this.departmentService = departmentService;
+    }
 
-	public List<Professor> findAll() {
-		return professorRepository.findAll();
-	}
+    public List<Professor> findAll() {
+        return professorRepository.findAll();
+    }
 
-	public Professor findById(Long id) {
-		return professorRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Professor not found"));
-	}
+    public Page<Professor> findAllWithPages(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
 
-	public List<Professor> findByName(String partName) {
-		return professorRepository.findByNameContainingIgnoreCase(partName);
-	}
+        return professorRepository.findAll(pageable).map(professor -> Professor
+                .builder()
+                .id(professor.getId())
+                .name(professor.getName())
+                .cpf(professor.getCpf())
+                .department(professor.getDepartment())
+                .build());
+    }
 
-	public List<Professor> findByDepartment(Long departmentId) {
-		Department department = new Department();
-		department.setId(departmentId);
-		return professorRepository.findByDepartment(department);
-	}
+    public Professor findById(Long id) {
+        return professorRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Professor not found"));
+    }
 
-	public Professor save(ProfessorRegisterDTO professor) {
+    public List<Professor> findByName(String partName) {
+        return professorRepository.findByNameContainingIgnoreCase(partName);
+    }
 
-		if (professorRepository.existsByCpf(professor.cpf())){
-			throw new AlreadyExistsException("Professor já existe");
-		}
+    public List<Professor> findByDepartment(Long departmentId) {
+        Department department = new Department();
+        department.setId(departmentId);
+        return professorRepository.findByDepartment(department);
+    }
 
-		Department department = departmentService.findById(professor.departmentId());
+    public Professor save(ProfessorRegisterDTO professor) {
 
-		Professor professorSave = Professor.builder()
-				.cpf(professor.cpf())
-				.name(professor.name())
-				.department(department)
-				.build();
-		return saveInternal(professorSave);
-	}
+        if (professorRepository.existsByCpf(professor.cpf())) {
+            throw new AlreadyExistsException("Professor já existe");
+        }
 
-	public Professor update(Professor professor) {
-		Long id = professor.getId();
+        Department department = departmentService.findById(professor.departmentId());
 
-		if (id == null || !professorRepository.existsById(id)) {
-			throw new InvalidProfessorException("Professor inválido");
-		}
+        Professor professorSave = Professor.builder()
+                .cpf(professor.cpf())
+                .name(professor.name())
+                .department(department)
+                .build();
+        return saveInternal(professorSave);
+    }
 
-		return saveInternal(professor);
-	}
+    public Professor update(Professor professor) {
+        Long id = professor.getId();
 
-	public void deleteById(Long id) {
-		if (professorRepository.existsById(id)) {
-			professorRepository.deleteById(id);
-		}
-	}
+        if (id == null || !professorRepository.existsById(id)) {
+            throw new InvalidProfessorException("Professor inválido");
+        }
 
-	private Professor saveInternal(Professor professor) {
-		if (professor.getDepartment() == null || professor.getDepartment().getId() == null) {
-			throw new InvalidDepartmentException();
-		}
+        return saveInternal(professor);
+    }
 
-		Department department = departmentService.findById(professor.getDepartment().getId());
-		if (department == null) {
-			throw new EntityNotFoundException("Departamento não encontrado.");
-		}
+    public void deleteById(Long id) {
+        if (professorRepository.existsById(id)) {
+            professorRepository.deleteById(id);
+        }
+    }
 
-		professor = professorRepository.save(professor);
-		professor.setDepartment(department);
+    private Professor saveInternal(Professor professor) {
+        if (professor.getDepartment() == null || professor.getDepartment().getId() == null) {
+            throw new InvalidDepartmentException();
+        }
 
-		return professor;
-	}
+        Department department = departmentService.findById(professor.getDepartment().getId());
+        if (department == null) {
+            throw new EntityNotFoundException("Departamento não encontrado.");
+        }
+
+        professor = professorRepository.save(professor);
+        professor.setDepartment(department);
+
+        return professor;
+    }
 }
